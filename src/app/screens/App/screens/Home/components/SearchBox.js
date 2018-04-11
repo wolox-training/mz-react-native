c03ca3121bf9bd78f9eae92c5345508ad7add50c
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-11 13:48:15 -0300

    Moved shared css to common file

--- src/app/screens/App/screens/Home/components/SearchBox.js
+++ src/app/screens/App/screens/Home/components/SearchBox.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./SearchBox.css";
 
 export default function SearchBox(props) {
-  return <input type="text" name="searchText" onChange={props.onChange} />;
+  return (
+    <input
+      type="text"
+      name="searchText"
+      onChange={props.onChange}
+      placeholder="Buscar..."
+      className="search"
+    />
+  );
 }
