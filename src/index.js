b2df6b4ec69d08b01a2f26144d41c84e051f04e6
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-11 15:29:58 -0300

    Add equalizer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/screens/App";
+import "./equalizer.css";
 
 import registerServiceWorker from "./registerServiceWorker";
 
