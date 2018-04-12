7a6b2e661ca5ae32562f3267b9d0feedbb194e26
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-12 15:21:42 -0300

    Use Book component in BookList

--- src/app/screens/App/screens/Home/components/BookList/index.js
+++ src/app/screens/App/screens/Home/components/BookList/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Book from '../Book';
+import './styles.css';
 
 export default function BookList(props) {
-  return props.books.map(book => <Book key={book.id} book={book} />);
+  return (
+    <div className="booklist">
+      {props.books.map(book => (
+        <Book key={book.id} book={book} className="q" />
+      ))}
+    </div>
+  );
 }
