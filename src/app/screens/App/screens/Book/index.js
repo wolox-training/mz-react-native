9f9c13f093fa264d162d3005e6b1abb2f18f4020
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-13 17:01:06 -0300

    Add goBack button to book details

--- src/app/screens/App/screens/Book/index.js
+++ src/app/screens/App/screens/Book/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import books from '../../../../../data/books.json';
+import './styles.css';
 
 class Book extends Component {
   render() {
     const bookID = parseInt(this.props.match.params.id, 10);
     const book = books.find(book => book.id === bookID);
 
-    return <h1>{book.title}</h1>;
+    return (
+      <div className="book">
+        <Link to={`/dashboard`}>Volver</Link>
+        <h1>{book.title}</h1>
+      </div>
+    );
   }
 }
 
