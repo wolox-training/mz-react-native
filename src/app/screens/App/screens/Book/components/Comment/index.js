fcddd2c65aa954c3d5b6f4b1705c8a5e415d516d
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-16 17:58:04 -0300

    Moved the comment layout to a different component

--- src/app/screens/App/screens/Book/components/Comment/index.js
+++ src/app/screens/App/screens/Book/components/Comment/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { PROFILE_IMAGE_ALT_TEXT } from '../../strings';
+import CommentLayout from '../CommentLayout';
 import './styles.css';
 import PropTypes from 'prop-types';
-import profileImage from '../../../../../../../assets/ASSETS/profile-pic.jpg';
 
 export default function Comment(props) {
   return (
-    <div className="comment-container">
-      <img src={props.image} className="comment-image" alt={PROFILE_IMAGE_ALT_TEXT} />
-      <div className="comment-info">
-        <span className="comment-name">{props.name}</span>
-        <span className="comment-date">{props.date}</span>
-        <span className="comment-content">{props.content}</span>
-      </div>
-    </div>
+    <CommentLayout>
+      <span className="comment-name">{props.name}</span>
+      <span className="comment-date">{props.date}</span>
+      <span className="comment-content">{props.content}</span>
+    </CommentLayout>
   );
 }
 
@@ -25,7 +21,6 @@ Comment.propTypes = {
 };
 
 Comment.defaultProps = {
-  image: profileImage,
   name: 'Harold',
   date: '20/03/18',
   content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.'
