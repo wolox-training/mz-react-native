9b868b6b4a334fded7cbb94288b378151cefddfb
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-16 16:55:22 -0300

    Added comments string

--- src/app/screens/App/screens/Book/strings.js
+++ src/app/screens/App/screens/Book/strings.js
@@ -3,3 +3,4 @@ export const RENT = 'Alquilar';
 export const LOREM_IPSUM =
   'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 export const SUGGESTIONS = 'Sugerencias';
+export const COMMENTS = 'Comentarios';
