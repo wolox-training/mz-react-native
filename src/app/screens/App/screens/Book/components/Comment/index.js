7cb78aad76ca8cca2c6fb2e2349b3fbce70e1450
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-16 17:27:08 -0300

    Added propTypes to comments

--- src/app/screens/App/screens/Book/components/Comment/index.js
+++ src/app/screens/App/screens/Book/components/Comment/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import './styles.css';
+import PropTypes from 'prop-types';
 import profileImage from '../../../../../../../assets/ASSETS/profile-pic.jpg';
 
 export default function Comment(props) {
   return (
     <div className="comment-container">
-      <img src={profileImage} className="comment-image" />
+      <img src={props.image} className="comment-image" />
       <div className="comment-info">
-        <span className="comment-name">Harold</span>
-        <span className="comment-date">20/03/18</span>
-        <span className="comment-content">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </span>
+        <span className="comment-name">{props.name}</span>
+        <span className="comment-date">{props.date}</span>
+        <span className="comment-content">{props.content}</span>
       </div>
     </div>
   );
 }
+
+Comment.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+  date: PropTypes.string,
+  content: PropTypes.string
+};
+
+Comment.defaultProps = {
+  image: profileImage,
+  name: 'Harold',
+  date: '20/03/18',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.'
+};
