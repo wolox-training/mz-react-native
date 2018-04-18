7350af12ee7a84b87c49854bad1ef06c33007515
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-18 15:55:59 -0300

    Make signup presentation component

--- src/app/screens/App/screens/Signup/index.js
+++ src/app/screens/App/screens/Signup/index.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import Signup from './layout';
 
 class SignupContainer extends Component {
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
-    return <Signup />;
+    return <Signup onInputChange={this.handleInputChange} />;
   }
 }
 
--- src/app/screens/App/screens/Signup/layout.js
+++ src/app/screens/App/screens/Signup/layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SIGNUP } from './strings';
+import { SIGNUP, PASSWORD_CONFIRMATION, FIRST_NAME, LAST_NAME } from './strings';
 import { EMAIL, PASSWORD } from '../../shared/strings';
 
 export default function Signup(props) {
@@ -25,6 +25,39 @@ export default function Signup(props) {
         type="password"
         className={`input ${props.passwordError ? 'error' : ''}`}
         name="password"
+        onChange={props.onInputChange}
+      />
+      {props.passwordError && <span className="error-message">{props.passwordError}</span>}
+      <label className="label" htmlFor="passwordConfirmation">
+        {PASSWORD_CONFIRMATION}
+      </label>
+      <input
+        required
+        type="password"
+        className={`input ${props.passwordError ? 'error' : ''}`}
+        name="passwordConfirmation"
+        onChange={props.onInputChange}
+      />
+      {props.passwordError && <span className="error-message">{props.passwordError}</span>}
+      <label className="label" htmlFor="firstName">
+        {FIRST_NAME}
+      </label>
+      <input
+        required
+        type="password"
+        className={`input ${props.passwordError ? 'error' : ''}`}
+        name="firstName"
+        onChange={props.onInputChange}
+      />
+      {props.passwordError && <span className="error-message">{props.passwordError}</span>}
+      <label className="label" htmlFor="lastName">
+        {LAST_NAME}
+      </label>
+      <input
+        required
+        type="password"
+        className={`input ${props.passwordError ? 'error' : ''}`}
+        name="lastName"
         onChange={props.onInputChange}
       />
       {props.passwordError && <span className="error-message">{props.passwordError}</span>}
