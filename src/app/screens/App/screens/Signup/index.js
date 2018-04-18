240fe7dbad0527b7ad146a3e1df406a37baf1f7a
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-18 17:05:12 -0300

    Validate inputs on signup

--- src/app/screens/App/screens/Signup/index.js
+++ src/app/screens/App/screens/Signup/index.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import Signup from './layout';
+import { EMAIL_ERROR, PASSWORD_ERROR } from '../../shared/strings';
+import { PASSWORD_CONFIRMATION_ERROR, FIRST_NAME_ERROR, LAST_NAME_ERROR } from './strings';
+import { emailIsValid, passwordIsValid, hasLettersOnly } from '../../../../utils/validationUtils';
 
 class SignupContainer extends Component {
+  state = {
+    emailError: false,
+    passwordError: false,
+    passwordConfirmationError: false,
+    firstNameError: false,
+    lastNameError: false
+  };
+
   handleInputChange = event => {
     const target = event.target;
     const value = target.value;
@@ -12,8 +23,51 @@ class SignupContainer extends Component {
     });
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    const email = this.state.email;
+    const password = this.state.password;
+    const passwordConfirmation = this.state.passwordConfirmation;
+    const firstName = this.state.firstName;
+    const lastName = this.state.lastName;
+
+    const validEmail = emailIsValid(email);
+    const validPassword = passwordIsValid(password);
+    const validPasswordConfirmation = password === passwordConfirmation;
+    const validFirstName = hasLettersOnly(firstName);
+    const validLastNameError = hasLettersOnly(lastName);
+
+    this.setState({
+      emailError: !validEmail && EMAIL_ERROR, //set either EMAIL_ERROR or false
+      passwordError: !validPassword && PASSWORD_ERROR,
+      passwordConfirmationError: !validPasswordConfirmation && PASSWORD_CONFIRMATION_ERROR,
+      firstNameError: !validFirstName && FIRST_NAME_ERROR,
+      lastNameError: !validLastNameError && LAST_NAME_ERROR
+    });
+
+    if (
+      validEmail &&
+      validPassword &&
+      validPasswordConfirmation &&
+      validFirstName &&
+      validLastNameError
+    ) {
+      this.requestAuthentication();
+    }
+  };
+
   render() {
-    return <Signup onInputChange={this.handleInputChange} />;
+    return (
+      <Signup
+        onInputChange={this.handleInputChange}
+        onSubmit={this.handleSubmit}
+        emailError={this.state.emailError}
+        passwordError={this.state.passwordError}
+        passwordConfirmationError={this.state.passwordConfirmationError}
+        firstNameError={this.state.firstNameError}
+        lastNameError={this.state.lastNameError}
+      />
+    );
   }
 }
 
