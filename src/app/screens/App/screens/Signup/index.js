7941a0c0e96a10a2f6d2767c61bf875ead016d9c
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-18 15:41:36 -0300

    Add singup screen

---
+++ src/app/screens/App/screens/Signup/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Signup from './layout';
+
+class SignupContainer extends Component {
+  render() {
+    return <Signup />;
+  }
+}
+
+export default SignupContainer;
---
+++ src/app/screens/App/screens/Signup/layout.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { SIGNUP } from './strings';
+import { EMAIL, PASSWORD } from '../../shared/strings';
+
+export default function Signup(props) {
+  return (
+    <form className="login" onSubmit={props.onSubmit}>
+      <h1 className="title">{SIGNUP}</h1>
+      <label className="label" htmlFor="email">
+        {EMAIL}
+      </label>
+      <input
+        required
+        type="email"
+        className={`input ${props.emailError ? 'error' : ''}`}
+        name="email"
+        onChange={props.onInputChange}
+      />
+      {props.emailError && <span className="error-message">{props.emailError}</span>}
+      <label className="label" htmlFor="password">
+        {PASSWORD}
+      </label>
+      <input
+        required
+        type="password"
+        className={`input ${props.passwordError ? 'error' : ''}`}
+        name="password"
+        onChange={props.onInputChange}
+      />
+      {props.passwordError && <span className="error-message">{props.passwordError}</span>}
+      <input type="submit" className="login-button" value={SIGNUP} onClick={props.onSubmit} />
+    </form>
+  );
+}
