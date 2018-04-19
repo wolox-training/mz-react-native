03f1f2472ae84c346147cc804448d781c3562e4a
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-19 18:07:03 -0300

    Move string to another file

--- src/app/screens/App/shared/strings.js
+++ src/app/screens/App/shared/strings.js
@@ -4,3 +4,4 @@ export const EMAIL = 'Email';
 export const PASSWORD = 'Contraseña';
 export const EMAIL_ERROR = 'Por favor ingresar un email válido';
 export const PASSWORD_ERROR = 'Por favor ingresar una contraseña válida';
+export const PROFILE_IMAGE_ALT_TEXT = 'Foto de perfil';
