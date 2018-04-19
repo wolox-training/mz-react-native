f1d6f4d1ab7d858659d9e5c9423addc889052063
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-19 16:03:54 -0300

    Add service for signing up a user

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,14 @@
 import { Api } from '../config/api';
 
 export const logIn = (email, password) => Api.post('users/sessions', { email, password });
+export const signUp = (email, password, confirm_password, first_name, last_name) =>
+  Api.post('users', {
+    user: {
+      email,
+      password,
+      confirm_password,
+      first_name,
+      last_name,
+      locale: 'en'
+    }
+  });
