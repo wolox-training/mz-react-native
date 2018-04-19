5ea250b8232f0d9f72cfcd6539bb9309056b88f3
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-19 18:07:29 -0300

    Start using new ProfileImage  component for book comments

--- src/app/screens/App/screens/Book/components/CommentLayout/index.js
+++ src/app/screens/App/screens/Book/components/CommentLayout/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { PROFILE_IMAGE_ALT_TEXT } from '../../strings';
-import profileImage from '../../../../../../../assets/ASSETS/profile-pic.jpg';
 import PropTypes from 'prop-types';
+import ProfileImage from '../../../../shared/components/ProfileImage';
 import './styles.css';
 
 export default function CommentLayout(props) {
   return (
     <div className="comment-container">
-      <img src={props.image} className="comment-image" alt={PROFILE_IMAGE_ALT_TEXT} />
+      <ProfileImage image={props.image} />
       <div className="comment-info">{props.children}</div>
     </div>
   );
@@ -15,8 +14,4 @@ export default function CommentLayout(props) {
 
 CommentLayout.propTypes = {
   image: PropTypes.string
-};
-
-CommentLayout.defaultProps = {
-  image: profileImage
 };
