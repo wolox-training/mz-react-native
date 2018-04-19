593a1dd54423ecf850359c782f999092a41eb51d
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-19 17:20:40 -0300

    Create navbar component

---
+++ src/app/screens/App/shared/components/NavBar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { LOGO_ALT_TEXT } from '../../strings';
+import './styles.css';
+import wBooksLogo from '../../../../../../assets/ASSETS/wbooks_logo.svg';
+import notifications from '../../../../../../assets/ASSETS/notifications.svg';
+import add_book from '../../../../../../assets/ASSETS/add_book.svg';
+
+export default function NavBar(props) {
+  return (
+    <div>
+      <nav className="navbar">
+        <img src={wBooksLogo} className="navbar-logo" alt={LOGO_ALT_TEXT} />
+        <div className="menu">
+          <img src={notifications} />
+          <img src={add_book} />
+          <img src={add_book} />
+        </div>
+      </nav>
+      {props.children}
+    </div>
+  );
+}
