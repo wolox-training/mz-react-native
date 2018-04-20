53cfc4d8c7a9e8515f0b264cb45c88863b9905b2
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-20 17:05:23 -0300

    Use Droppable component in

--- src/app/screens/App/shared/components/NavBar/index.js
+++ src/app/screens/App/shared/components/NavBar/index.js
@@ -7,6 +7,7 @@ import {
 } from '../../strings';
 import ProfileImage from '../ProfileImage';
 import Notifications from '../Notifications';
+import Droppable from '../Droppable';
 import './styles.css';
 import wBooksLogo from '../../../../../../assets/ASSETS/wbooks_logo.svg';
 import notifications from '../../../../../../assets/ASSETS/notifications.svg';
@@ -14,17 +15,19 @@ import add_book from '../../../../../../assets/ASSETS/add_book.svg';
 import ProfileOptions from '../ProfileOptions';
 
 class NavBar extends Component {
-  state = { showProfileOptions: false, toggleShowNotifications: false };
+  state = { showProfileOptions: false, showNotifications: false };
 
   toggleShowProfileOptions = () => {
     this.setState(prevState => ({
-      showProfileOptions: !prevState.showProfileOptions
+      showProfileOptions: !prevState.showProfileOptions,
+      showNotifications: false
     }));
   };
 
   toggleShowNotifications = () => {
     this.setState(prevState => ({
-      toggleShowNotifications: !prevState.toggleShowNotifications
+      showNotifications: !prevState.showNotifications,
+      showProfileOptions: false
     }));
   };
 
@@ -35,15 +38,17 @@ class NavBar extends Component {
           <img src={wBooksLogo} className="navbar-logo" alt={LOGO_ALT_TEXT} />
         </Link>
         <div className="menu">
-          <img
-            src={notifications}
-            alt={NOTIFICATIONS_ICON_ALT_TEXT}
-            onClick={this.toggleShowNotifications}
-          />
-          {this.state.toggleShowNotifications && <Notifications />}
+          <Droppable showDropdown={this.state.showNotifications} dropdown={<Notifications />}>
+            <img
+              src={notifications}
+              alt={NOTIFICATIONS_ICON_ALT_TEXT}
+              onClick={this.toggleShowNotifications}
+            />
+          </Droppable>
           <img src={add_book} alt={BOOK_SUGGESTIONS_ICON_ALT_TEXT} />
-          <ProfileImage onClick={this.toggleShowProfileOptions} />
-          {this.state.showProfileOptions && <ProfileOptions />}
+          <Droppable showDropdown={this.state.showProfileOptions} dropdown={<ProfileOptions />}>
+            <ProfileImage onClick={this.toggleShowProfileOptions} />
+          </Droppable>
         </div>
       </nav>
     );
