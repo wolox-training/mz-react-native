64faa7c5af01054831115082a0fde0b01303443f
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-20 13:20:21 -0300

    Move Navbar to PrivateRoutes only

--- src/app/screens/App/components/CustomRoute/index.js
+++ src/app/screens/App/components/CustomRoute/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect, Route } from 'react-router-dom';
-import NavBar from '../../shared/components/NavBar';
 
 const CustomRoute =  ({ component: Component, ...rest, isAllowedAccess, redirectPath }) => (
   <Route
     {...rest}
     render={props =>
-      isAllowedAccess ? (
-        <div>
-          <NavBar/>
+      isAllowedAccess ? 
           <Component {...props} />
-        </div>
-      ) : (
+       : (
         <Redirect
           to={{
             pathname: redirectPath,
