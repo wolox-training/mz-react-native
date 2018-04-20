75fda92206e92f2dd62baa4e5336bc75bf1cefd3
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-20 13:39:05 -0300

    Add onClick prop to ProfileImage

--- src/app/screens/App/shared/components/ProfileImage/index.js
+++ src/app/screens/App/shared/components/ProfileImage/index.js
@@ -4,12 +4,15 @@ import { PROFILE_IMAGE_ALT_TEXT } from '../../strings';
 import PropTypes from 'prop-types';
 import './styles.css';
 
-export default function ProfileImage({ image }) {
-  return <img src={image} className="profile-image" alt={PROFILE_IMAGE_ALT_TEXT} />;
+export default function ProfileImage({ image, onClick }) {
+  return (
+    <img src={image} className="profile-image" alt={PROFILE_IMAGE_ALT_TEXT} onClick={onClick} />
+  );
 }
 
 ProfileImage.propTypes = {
-  image: PropTypes.string
+  image: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 ProfileImage.defaultProps = {
