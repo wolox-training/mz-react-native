27c20909e02216defc5a1811fc484983a19857e4
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-24 16:47:43 -0300

    Add store provider

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/screens/App';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
 import './equalizer.css';
-
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
