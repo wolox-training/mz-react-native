fe8ed39eaee33faa5c0a431b68474dccad1efa7d
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-25 12:46:55 -0300

    Add reducer for dropdown menues

---
+++ src/Redux/droppableMenues/reducer.js
@@ -0,0 +1,19 @@
+import Immutable from 'immutable';
+import { actions } from './actions';
+
+const defaultState = { showingNotifications: false, showingProfileOptions: false };
+
+export default function reducer(state = Immutable.Map(defaultState), action) {
+  switch (action.type) {
+    case actions.TOGGLE_SHOW_NOTIFICATIONS:
+      return state
+        .set('showingNotifications', !state.get('showingNotifications'))
+        .set('showingProfileOptions', false);
+    case actions.TOGGLE_SHOW_PROFILE_OPTIONS:
+      return state
+        .set('showingProfileOptions', !state.get('showingProfileOptions'))
+        .set('showingNotifications', false);
+    default:
+      return state;
+  }
+}
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import books from './books/reducer';
+import droppableMenues from './droppableMenues/reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducer = combineReducers({
-  books
+  books,
+  droppableMenues
 });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
