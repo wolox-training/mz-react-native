7063357b674bc5d245d153aed59b2ce8c4d5f4f6
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-25 13:00:55 -0300

    Add failure on getting books reducer

--- src/Redux/books/reducer.js
+++ src/Redux/books/reducer.js
@@ -7,6 +7,9 @@ export default function reducer(state = Immutable.Map(defaultState), action) {
   switch (action.type) {
     case actions.GET_BOOKS_SUCCESS:
       return state.merge({ books: action.payload.data });
+    case actions.GET_BOOKS_FAILURE:
+      console.error(action.payload.err);
+      return state;
     default:
       return state;
   }
