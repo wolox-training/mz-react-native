8e9ac00f1ca7f2d5a0dd6f94a821c5d2b2f42016
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-26 17:38:00 -0300

    Remmove unnecesary styles from home screen

--- src/app/screens/TodoList/styles.js
+++ src/app/screens/TodoList/styles.js
@@ -4,13 +4,6 @@ import { green } from '../../../constants/colors';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  mainButton: {
-    backgroundColor: green,
-    padding: 10,
-    borderRadius: 3
+    flex: 1
   }
 });
