84cfedbf9ca2f12f1c6c3c17c99da48bddebea76
Mariano Zicavo <marianozicavo@gmail.com>
2018-04-27 17:34:20 -0300

    Moved strings to another file

--- src/app/screens/TodoList/components/Todo/index.js
+++ src/app/screens/TodoList/components/Todo/index.js
@@ -1,6 +1,8 @@
 import { View, Text, Button } from 'react-native';
 import React from 'react';
 
+import { DONE, DELETE } from '../../strings';
+
 import styles from './styles';
 
 export default function Todo({ onDelete, onToggleComplete, toDo }) {
@@ -8,8 +10,8 @@ export default function Todo({ onDelete, onToggleComplete, toDo }) {
     <View style={styles.toDoContainer}>
       <Text style={toDo.completed && styles.toDoText}>{toDo.text}</Text>
       <View style={styles.toDoOptions}>
-        <Button onPress={() => onToggleComplete(toDo.id)} title="Done" />
-        <Button onPress={() => onDelete(toDo.id)} title="Delete" />
+        <Button onPress={() => onToggleComplete(toDo.id)} title={DONE} />
+        <Button onPress={() => onDelete(toDo.id)} title={DELETE} />
       </View>
     </View>
   );
